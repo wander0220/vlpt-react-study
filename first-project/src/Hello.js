7da3f63d13bf2f5063b795916e375ff705471dd0
Suhyeon Kim <s2018s26@e-mirim.hs.kr>
2022-05-10 22:24:05 +0900

    [Update] #1 JSX의 기본 규칙 알아보기

--- first-project/src/Hello.js
+++ first-project/src/Hello.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Hello(){
+    const name = '뫄뫄님';
+    const style = {
+        backgroundColor: 'black',
+        color: 'white',
+        fontSize: 24,
+        padding: '1rem'
+    }
     return (
         <>
-            <div>안녕하세요</div>
+            <div className = "hi" style={style}>{name} 안녕하세요</div>
         </>
     )
 }

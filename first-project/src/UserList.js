99572637daa1d9568627851a042197c677765782
Suhyeon Kim <s2018s26@e-mirim.hs.kr>
2022-06-06 20:43:06 +0900

    [Add] #1 22. Context API 를 사용한 전역 값 관리
    React.createContext()로 Context API 생성하고 Provider로 감싸진 컴포넌트면 Context 값을 다른 곳에서 조회해서 쓸 수 있다. (import하면 됨!)

--- first-project/src/UserList.js
+++ first-project/src/UserList.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
+import { UserDispatch } from "./App";
 
-const User = React.memo(function ({ user, onRemove, onToggle}) {
+const User = React.memo(function ({ user}) {
+    const dispatch = useContext(UserDispatch);
     useEffect(() => {
         console.log(user);
     });
@@ -11,10 +13,10 @@ const User = React.memo(function ({ user, onRemove, onToggle}) {
                     cursor : 'pointer',
                     color: user.active ? 'green' : 'black'
                 }}
-                onClick={e => onToggle(user.id)}
+                onClick={e => { dispatch({type: 'TOGGLE_USER', id:user.id})}}
             >{user.username}</b>
             <span>{user.email}</span>
-            <button onClick={() =>onRemove(user.id)}>삭제</button>
+            <button onClick={() =>{ dispatch({type:'REMOVE_USER', id:user.id})}}>삭제</button>
         </div>
     )
 });
@@ -25,12 +27,7 @@ function UserList({users,onRemove, onToggle}) {
         <>
         {
             users && users.map((user,index) =>(
-                <User 
-                    user={user}
-                    key={user.id}
-                    onRemove={onRemove}
-                    onToggle={onToggle}
-                />
+                <User user={user} key={user.id} />
             ))
         }
         </>

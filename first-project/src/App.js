50603ba7981d2c2bfa474fe5ac1047cc977a094e
Suhyeon Kim <s2018s26@e-mirim.hs.kr>
2022-06-06 20:54:34 +0900

    [Add] #1 22 숙제

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -55,52 +55,16 @@ function reducer(state, action) {
 export const UserDispatch = React.createContext(null);
 
 function App() {
-  const [{ username, email }, onChange, reset] = useInputs({
-    username: '',
-    email: ''
-  });
   const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
   const { users } = state;
 
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    reset();
-    nextId.current += 1;
-  }, [username, email, reset]);
-
-  // const onToggle = useCallback(id => {
-  //   dispatch({
-  //     type: 'TOGGLE_USER',
-  //     id
-  //   });
-  // }, []);
-
-  // const onRemove = useCallback(id => {
-  //   dispatch({
-  //     type: 'REMOVE_USER',
-  //     id
-  //   });
-  // }, []);
-
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <UserDispatch.Provider value ={dispatch}>
-        <CreateUser
-          username={username}
-          email={email}
-          onChange={onChange}
-          onCreate={onCreate}
-        />
+        <CreateUser/>
         <UserList users={users} />
         <div>활성사용자 수 : {count}</div>
       </UserDispatch.Provider>
